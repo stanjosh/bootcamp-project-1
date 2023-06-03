19b9dc38038d1af97776585dd2fe5f9017395540
Joshua Stanford <stanjosh@gmail.com>
2023-06-03 11:32:35 -0500

    rps api call working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,23 @@
 
-const apiUrl = "https://api.publicapis.org/entries"
-
-
-async function getApis() {
-    var resp = await fetch(apiUrl)
-    var data = await resp.json()
-    console.log(data.entries)
-    return data.entries
-}
-
-async function drawApis() {
-    var entries = await getApis()
-    for (var i in entries) {
-        var row = $(`<tr></tr>`)
-        var cell1 =$(
-            `<td><a href=${entries[i].Link}>${entries[i].API}</a></td>`)
-        var cell2 =$(`<td>${entries[i].Description}</td>`)
-        var cell3 =$(`<td>${entries[i].Category}</td>`)
-        $('#apiTable').append(row)
-        $(row).append(cell1)
-        $(row).append(cell2)
-        $(row).append(cell3)
+async function rpsRequest(playerMove) {
+    return {
+        async: true,
+        crossDomain: true,
+        url: `https://rock-paper-scissors7.p.rapidapi.com/?choice=${playerMove}`,
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '64cfafa8dbmsh4b77b2988b4bddep15f639jsn621283b6b4c6',
+            'X-RapidAPI-Host': 'rock-paper-scissors7.p.rapidapi.com'
+    	}
     }
-}
+};
 
-const techyUrl = "https://techy-api.vercel.app/api/json";
-const nasaUrl = "https://epic.gsfc.nasa.gov/api/enhanced"
-
-async function newAdvice() {
-    const resp = await fetch(techyUrl);
-    const quote = await resp.json()
-    $('#quote').text(`"${quote.message}!"`)
-}
-
-async function newImage() {
-    $('body').addClass('clear')   
-    var index = Math.floor(Math.random() * 19)
-    const resp = await fetch(nasaUrl);
-    await resp.json()
-    .then((images) => {
-        var dateTime = dayjs(images[index].date)
-        var day = dateTime.format('DD')
-        var month = dateTime.format('MM')
-        var year = dateTime.format('YYYY')
-        var image = `https://epic.gsfc.nasa.gov/archive/enhanced/${year}/${month}/${day}/png/${images[index].image}.png`
-        $('body').css('background-image', `url("${image}")`)
-        newAdvice()
-        setTimeout(() => {
-
-            $('body').removeClass('clear');
-        }, 1500)
-
-    
-    })
-}
-    
-
-$('#newStuff').on('click', () => {
-    newImage()
-    drawApis()
+  
+$('#playerButtons').on('click', async (event) => {
+    settings = await rpsRequest($(event.target).data('move'))
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
 })
 
 
